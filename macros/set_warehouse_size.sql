b246f537788afcbf7fe72fe6498fbedf3c0e007f
Jurri Gerretsen <62351599+jurrigerretsen@users.noreply.github.com>
2024-06-18 15:41:12 +0200

    ✨ Set warehouse size improvements

--- macros/set_warehouse_size.sql
+++ macros/set_warehouse_size.sql
@@ -8,23 +8,24 @@
 
 {% macro snowflake__set_warehouse_size(size) %}
 
-	{% if var("available_sizes", None) == None %}
-		{{ exceptions.raise_compiler_error("Please set the `available_sizes` variable in the dbt_project.yml.") }}
+	{% if var("snowflake_warehouses", None) == None %}
+		{{ exceptions.raise_compiler_error("Please set the `snowflake_warehouses` variable in the dbt_project.yml.") }}
 	{% endif %}
 
-    {% if size not in var("available_sizes") %}
-        {{ exceptions.raise_compiler_error("Warehouse size not one of " ~ var("available_sizes")) }}
-    {% endif %}
-
-    {% if target.name == 'prod' %}
-        {{ return('PROD_DBT_WH_' ~ size) }}
-    {% elif target.name == 'dev' %}
-        {{ return('DEV_DBT_WH_' ~ size) }}
-    {% elif target.name == 'ci' %}
-        {{ return('CI_WH_' ~ size) }}
+    {% set size_dict = var('snowflake_warehouses') %}
+    {% set target_name = target.name %}
+    
+    {% if target.name in size_dict %}
+        {% set env = size_dict[target_name] %}
+        {% if env['size']|length == 0 %}
+            {{ env['warehouse_prefix'] }}
+        {% elif size in env['size'] %}
+            {{ env['warehouse_prefix'] ~ size }}
+        {% else %}
+            {{ exceptions.raise_compiler_error("Size '" ~ size ~ "' is not valid for the environment '" ~ target_name ~ "'.") }}
+        {% endif %}
     {% else %}
-		{% do log('Unknown target - running with default warehouse ' ~ target.warehouse, True) %}
-        {{ return(target.warehouse) }}
+        {{ exceptions.raise_compiler_error("Target name '" ~ target_name ~ "' does not match any environment key.") }}
     {% endif %}
 
 {% endmacro %}
