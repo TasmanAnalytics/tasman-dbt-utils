d35067f6bbea397670047f0491117b5029dcf402
Ho Yin Wong <55826777+Zshyw@users.noreply.github.com>
2025-03-17 11:54:10 +0000

    Add ability to aggregate by list of dimensions in source comparison tests

--- tests/generic/test_sum_matches_source.sql
+++ tests/generic/test_sum_matches_source.sql
@@ -1,39 +1,56 @@
 {% test sum_aggregate_matches_source(model, column_name, source_model, source_field, source_metric, comparison_field, percent_mismatch_threshold = 0) %}
 
+{% set max_index = comparison_field|length %}
+
 with src as (
     select
-        {{ source_field }} as date,
-        {{ source_metric }} as measure_src
+        {% for dimension in source_field %}
+        {{ dimension }} as dim_{{ loop.index }},
+        {% endfor %}
+        sum({{ source_metric }}) as measure_src
 
     from {{ source_model }}
+    group by
+        {% for dimension in source_field %}
+        {{ dimension }}{% if not loop.last %},{% endif %}
+        {% endfor %}
 ),
 
 new_model as (
     select
-        {{ comparison_field }} as date,
+        {% for dimension in comparison_field %}
+        {{ dimension }} as dim_{{ loop.index }},
+        {% endfor %}
         sum({{ column_name }}) as measure_comparison
 
     from {{ model }}
 
-    group by date
+    group by
+        {% for dimension in comparison_field %}
+        {{ dimension }}{% if not loop.last %},{% endif %}
+        {% endfor %}
 ),
 
 comparison as (
     select
-        coalesce(src.date, new_model.date) as date,
+        {% for num in range(1, max_index + 1) %}
+            coalesce(src.dim_{{ i }}, new_model.dim_{{ i }}) as dim_{{ num }},
+        {% endfor %}
         src.measure_src,
         new_model.measure_comparison,
         case
-        	when src.measure_src = 0 then src.measure_src = new_model.measure_comparison
+        	when coalesce(src.measure_src, 0) = 0 then src.measure_src = new_model.measure_comparison
             -- for scenarios where source_value = 0 and comparison_value = 0, this will return true
         	-- for scenarios where source_value = 0 and comparison_value = ±1+, this will return false no matter the threshold
-            when src.measure_src != 0 then abs(round((div0(new_model.measure_comparison, src.measure_src) - 1), 3)) <= ( {{ percent_mismatch_threshold }} / 100.0 )
+            when src.measure_src != 0 then abs(round(((new_model.measure_comparison / nullif(src.measure_src, 0)) - 1), 3)) <= ( {{ percent_mismatch_threshold }} / 100.0 )
             -- for scenarios where source_value ±1+ and comparison_value = ±1+ so the div0 function can work
             -- this will return false if the percent difference between both values are above error threshold set in yml file
         else false
         end as is_match
     from src
-    full outer join new_model on new_model.date = src.date
+    full outer join new_model 
+        on {% for i in range(1, max_index + 1) %} src.dim_{{ i }} = new_model.dim_{{ i }} {% if not loop.last %} and {% endif %}{% endfor %}
+
 )
 
 select * from comparison
